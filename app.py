099f6a24980800d7ef1413b2416fc04a7bfc90db
nschuelke1 <ndschuelke@wisc.edu>
2025-08-13 19:17:06 -0500

    a

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ url = up.urlparse(os.environ["DATABASE_URL"])
 
 # Connect to PostgreSQL
 conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
+    database=url.path[1:],        
+    user=url.username,            
+    password=url.password,         
+    host=url.hostname,             
+    port=url.port                  
 )
 
 # Serve frontend
